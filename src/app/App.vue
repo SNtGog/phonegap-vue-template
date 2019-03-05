3b4d5001b4491652217bf5877b83eecfdcd330b4
Gog <gogntp@gmail.com>
2019-03-05 17:58:58 +0400

    upd

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -19,7 +19,7 @@ export default class App extends Vue {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
