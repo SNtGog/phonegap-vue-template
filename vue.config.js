a3f228203e030c5bfaf3391124c40b9fbf5837bc
Gog <gogntp@gmail.com>
2019-03-06 02:07:15 +0400

    upd

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,7 @@ var TypeScriptAsset = require('parcel-bundler/src/assets/TypeScriptAsset.js')
 var autoprefixer = require('autoprefixer');
 
 module.exports = {
-  publicPath: '',
+  publicPath: './',
   extractCSS: true,
-  postcss: [autoprefixer()],
-  customCompilers: {
-    ts: function(content, cb, compiler, filePath) {
-      let ts = new TypeScriptAsset(filePath, {}, { rootDir: filePath })
-      ts.contents = content
-      ts.process().then(res => {
-        cb(null, res.js)
-      })
-    }
-  }
+  postcss: [autoprefixer()]
 }
